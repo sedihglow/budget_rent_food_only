d691a31a0f2bcc33ed9ae3e195f336385d52d4b6
J.R <jamesmross4@gmail.com>
2023-08-28 11:55:34 -0700

    Create Minimal_food_home_only.c
    Just setting up the skeleton so see how much money i need. Already have the bare min in the readme and comment. Have you ever spent less than 15 dollars for a comfortable meal that fills you up for awhile?
    
    3 meals a day at $15 even. 15000 a month in rent. After taxes to not starve to death and have a home, $34,200 after taxes. Do you make that AFTER TAXES??

---
+++ Minimal_food_home_only.c
@@ -0,0 +1,11 @@
+/*
+* 3 meals a day at $15 even. 15000 a month in rent. After taxes to not starve to death and have a home, $34,200 after taxes.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+  printf("your an idiot."); return 0;
+}
